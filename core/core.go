5d5775488d1c22de0de8f7f8475a2123cb57970d
betall <001vk001@gmail.com>
2017-11-03 14:11:02 +0000

    add core

---
+++ core/core.go
@@ -0,0 +1,10 @@
+package core
+
+//DocumentUID values represent the id of a document
+type DocumentUID uint32
+
+//Document values represent a document that can be indexed and queried
+type Document struct {
+	UID DocumentUID
+	Attributes map[string]string
+}
